2e93344aabb8a83528988d389d8d6e990b7156b6
Joachim Bengtsson <joachimb@gmail.com>
2010-07-18 11:00:00 +0200

    Meh, I'm not using CoreData anyway. I wish I was, but I'm not. I'll just put all this back someday.

--- OverTask_AppDelegate.h
+++ OverTask_AppDelegate.h
@@ -8,21 +8,11 @@
 
 @interface OverTask_AppDelegate : NSObject 
 {
-    NSWindow *window;
+  NSWindow *window;
     
-    NSPersistentStoreCoordinator *persistentStoreCoordinator;
-    NSManagedObjectModel *managedObjectModel;
-    NSManagedObjectContext *managedObjectContext;
-	
 	IBOutlet TaskView *task;
 }
 @property (readonly) TaskView *task;
 @property (nonatomic, retain) IBOutlet NSWindow *window;
-
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-
-- (IBAction)saveAction:sender;
 
 @end
